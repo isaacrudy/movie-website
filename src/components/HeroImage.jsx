35ea913db6b27e96e7ce4446aac597a48f621caa
yummiestjam <email@email.com>
2025-05-25 19:14:01 -0700

    added hero image styles & content for PC size screens, and tweaked header styles to fit it better. no mobile fixes yet

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import { IMG_BASE } from "../utils/api";
+import { formatMovieDate } from "../utils/toolbelt";
 import { Link, useNavigate } from "react-router-dom";
+import "../styles/heroimage.css";
+import FavoriteButton from "./FavoriteButton";
 
 function HeroImage({ movieData }) {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <section
       onClick={() => {
         navigate(`/movie/${movieData.id}`);
       }}
-        className="hero-image"
+      className="hero-image"
     >
       <img
-        src={`${IMG_BASE}/w342/${movieData.backdrop_path}`}
-        alt={movieData.overview}
+        src={`${IMG_BASE}/w1280/${movieData.backdrop_path}`}
+        alt={movieData.title + " backdrop as hero image"}
       />
+      <div className="hero-gradient">
+        <div className="hero-title">
+          <FavoriteButton movieData={movieData} />
+          <h2 className="title">{movieData.title}</h2>
+        </div>
+        <p className="release">{formatMovieDate(movieData.release_date)}</p>
+        <p className="summary">
+          {movieData.overview}...
+        </p>
+      </div>
     </section>
   );
 }
